0ea55ae66465a1f9c5a6e9d4d784d80ad7bebf9f
Aleksandr <platonenkov87@gmail.com>
2022-09-18 12:46:13 +0300

    trustline extensions

--- Xrpl.C/Xrpl/Client/Extensions/TrustlineExtensions.cs
+++ Xrpl.C/Xrpl/Client/Extensions/TrustlineExtensions.cs
@@ -5,15 +5,43 @@ namespace xrpl_c.Xrpl.Client.Extensions
 {
     public static class TrustlineExtensions
     {
+        // maxSupply = 999999999999999900000000000000000000000000000000000000000000000000000000000000000000000000000000
+
         /// <summary>
         /// overrides the value to the maximum allowed for trustline installation if it greater that maximum
         /// </summary>
-        /// <param name="value">value for the installation</param>
+        /// <param name="value">
+        /// value for the installation<br/>
+        /// maxSupply = 999999999999999900000000000000000000000000000000000000000000000000000000000000000000000000000000</param>
         /// <returns>valid value</returns>
         public static string ToScientfic(this string value)
         {
             var num = double.Parse(value, (NumberStyles.Float & NumberStyles.AllowExponent)| NumberStyles.AllowExponent | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
             var valid = $"{num:################e00}";
+            return valid;
+        }
+        /// <summary>
+        /// overrides the value to the maximum allowed for trustline installation if it greater that maximum
+        /// </summary>
+        /// <param name="value">
+        /// value for the installation<br/>
+        /// maxSupply = 999999999999999900000000000000000000000000000000000000000000000000000000000000000000000000000000</param>
+        /// <returns>valid value</returns>
+        public static string ToScientfic(this double value)
+        {
+            var valid = $"{value:################e00}";
+            return valid;
+        }
+        /// <summary>
+        /// overrides the value to the maximum allowed for trustline installation if it greater that maximum
+        /// </summary>
+        /// <param name="value">
+        /// value for the installation<br/>
+        /// maxSupply = 999999999999999900000000000000000000000000000000000000000000000000000000000000000000000000000000</param>
+        /// <returns>valid value</returns>
+        public static string ToScientfic(this decimal value)
+        {
+            var valid = $"{value:################e00}";
             return valid;
         }
     }
